65a0ba331628054d98b3c821a8ff690c358d07db
GustavoPisanello <99992149+GustavoPisanello@users.noreply.github.com>
2023-10-23 10:44:26 -0300

    Add files via upload

--- Organizarty - OnePage/js/script.js
+++ Organizarty - OnePage/js/script.js
@@ -24,20 +24,36 @@ window.addEventListener("scroll", () => {
 
 
 let cont = document.querySelector('#Events');
-let layer = document.querySelectorAll('.txtEvents');
+let texto = document.querySelectorAll('.txtEvents');
+let baloes = document.querySelector("#baloes");
+let bolas = document.querySelector("#bolas");
+let img = document.querySelectorAll(".imgEvents")
 
 function AnimacaoTela1(){
     cont.onscroll = function(){
         let X = cont.scrollTop;
         console.log("scroll lado: " + cont.scrollTop);
-        layer[0].style.left = X/4 + 'px';
-        layer[1].style.left = X/ + 'px';
-        layer[2].style.left = X/ + 'px';
-        layer[3].style.left = X/ + 'px';
-        layer[4].style.left = X/ + 'px';
-}
+        if(cont.scrollTop < 1600){
+            texto[0].style.right = X/2 + 'px';
+            baloes.style.top = "-" + X/2 + "px";
+            bolas.style.height = 80 + X/16 + "vh";
+            texto[0].classList.add("inversaoTexto");
+            img[0].classList.add("inversaoImg");
+            if(X == 0){
+                texto[0].classList.remove("inversaoTexto");
+                img[0].classList.remove("inversaoImg");
+            }
+        } else if(cont.scrollTop < 3200){
+            X = cont.scrollTop - 1600;
+            texto[1].style.left = X/4 + 'px';
+        } else if(cont.scrollTop < 4800){
+            X = cont.scrollTop - 3200;
+            texto[2].style.left = X/4 + 'px';
+        } else if(cont.scrollTop < 6400){
+            X = cont.scrollTop - 4800;
+            texto[3].style.left = X/4 + 'px';
+        }
+    }
 }
 
 AnimacaoTela1();
-
-
